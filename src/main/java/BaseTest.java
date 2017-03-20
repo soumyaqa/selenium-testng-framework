c9b9dfb6d7d44247144c64d8304275c28ed46196
guna <gunasekaran_p@homedepot.com>
2017-03-20 15:36:42 -0400

    Report implemented

---
+++ src/main/java/BaseTest.java
@@ -0,0 +1,41 @@
+/**
+ * Created by GXP8655 on 3/20/2017.
+ */
+import java.lang.reflect.Method;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+
+/*
+ * This is a base Test class. All testng tests needs to inherited from
+ * this class. It contains common reporting infra
+ */
+public class BaseTest {
+    private String testName;
+    private String testDescription;
+
+    @BeforeMethod
+    public void beforeMethod(Method caller) {
+        //TODO : Get the test name & description from Excelsheet and pass it to the getTest method
+        testName = caller.getName();
+        testDescription = "This is a simple test case description";
+        Reports.getTest(testName, testDescription);
+    }
+
+    @AfterMethod
+    public void afterMethod(Method caller) {
+        Reports.closeTest(caller.getName());
+    }
+    /*
+     * After suite will be responsible to close the report properly at the end
+     * You an have another afterSuite as well in the derived class and this one
+     * will be called in the end making it the last method to be called in test exe
+     */
+    @AfterSuite
+    public void afterSuite() {
+        Reports.closeReport();
+    }
+
+}
---
+++ src/test/java/TestCase.java
@@ -0,0 +1,22 @@
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import org.testng.annotations.Test;
+
+/**
+ * Created by GXP8655 on 3/20/2017.
+ */
+
+public class TestCase extends BaseTest {
+
+    @Test
+    public void test001() {
+        ExtentTest testReporter = Reports.getTest();
+        testReporter.log(LogStatus.INFO, "first test case");
+    }
+    @Test
+    public void test002() {
+        long startTime = System.nanoTime();
+        ExtentTest testReporter = Reports.getTest();
+        testReporter.log(LogStatus.INFO, "second test case");
+    }
+}
