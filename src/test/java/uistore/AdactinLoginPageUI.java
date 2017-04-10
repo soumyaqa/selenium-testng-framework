632b34e1e21b600413d22cf4ad46cc1083c5e25f
Udhayamca <udhaya.ironcity@gmail.com>
2017-04-10 19:47:06 +0530

    elements for login page mapped

---
+++ src/test/java/uistore/AdactinLoginPageUI.java
@@ -0,0 +1,12 @@
+package uistore;
+
+import org.openqa.selenium.By;
+
+/**
+ * Created by Udhayakumar on 4/10/2017.
+ */
+public class AdactinLoginPageUI {
+    public static final By userName = By.id("username");
+    public static final By passWord = By.id("password");
+    public static final By login = By.id("login");
+}
