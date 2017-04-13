06d5435320e4a9a6a7ad35cebeb5c563b78aba9d
Udhayamca <udhaya.ironcity@gmail.com>
2017-04-13 18:14:03 +0530

    test case for searchHotel page

---
+++ src/test/java/tests/Test_02_SearchHotel.java
@@ -0,0 +1,21 @@
+package tests;
+
+import framework.BaseTest;
+import org.testng.annotations.Test;
+import pages.AdactinHomePage;
+
+/**
+ * Created by Udhayakumar on 4/10/2017.
+ */
+public class Test_02_SearchHotel extends BaseTest {
+    AdactinHomePage loginPage;
+
+    @Test
+    public void setSearchPage() {
+        loginPage = new AdactinHomePage(driver, testReporter, element);
+        openApplication("http://www.adactin.com/HotelApp/");
+        loginPage.login("home1234", "root1234")
+                .searchPage("Sydney", "Hotel Creek", "Standard", "1 - One");
+
+    }
+}
